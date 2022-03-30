6216c02d243ed1f4434351ed04c96b644ba30ee3
Gourav kumar Singh <63281468+gouravsingh675@users.noreply.github.com>
2022-03-30 23:03:35 +0530

    Add files via upload

---
+++ Portfolio website Gourav/js/app.js
@@ -0,0 +1,13 @@
+//Header fixed
+
+window.onscroll = function () {
+  const docScrollTop = document.documentElement.scrollTop;
+
+  if (window.innerWidth > 991) {
+    if (docScrollTop > 100) {
+      document.querySelector("header").classList.add("fixed");
+    } else {
+      document.querySelector("header").classList.remove("fixed");
+    }
+  }
+}
